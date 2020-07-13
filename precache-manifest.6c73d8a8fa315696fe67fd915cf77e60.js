4f6f6762b9e3556a9f87513a8e91602581960094
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2020-07-13 19:55:15 -0300

    Updates

---
+++ precache-manifest.6c73d8a8fa315696fe67fd915cf77e60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea2a206451847515ce30b07dffe93820",
+    "url": "/abrirbiscoito/index.html"
+  },
+  {
+    "revision": "b0bbdd8d26203b2fee41",
+    "url": "/abrirbiscoito/static/css/main.b975d8d9.chunk.css"
+  },
+  {
+    "revision": "b2448abe8aeaefce0a64",
+    "url": "/abrirbiscoito/static/js/2.32d81f7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/abrirbiscoito/static/js/2.32d81f7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0bbdd8d26203b2fee41",
+    "url": "/abrirbiscoito/static/js/main.4b62faff.chunk.js"
+  },
+  {
+    "revision": "58371ac63cfd8cfb692d",
+    "url": "/abrirbiscoito/static/js/runtime-main.1bdc30f3.js"
+  },
+  {
+    "revision": "a2bd231ca8e65322172513b0b27e00b7",
+    "url": "/abrirbiscoito/static/media/biscoito.a2bd231c.png"
+  }
+]);
